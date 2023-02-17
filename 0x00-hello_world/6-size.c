86c4c44907a6a5e2d6366703f8dfa1f680c8e2a3
chiomaibeh <augustaibeh25@gmail.com>
2023-02-17 01:33:40 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main-The parent function of the C program
+ *
+ * Return: Gives you back the void 0 you gave it
+ */
+
+
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
+	return (0);
+}

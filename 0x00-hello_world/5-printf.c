9d4a4e3a1cc3779a2760ef8b81fbe3f46d886adb
chiomaibeh <augustaibeh25@gmail.com>
2023-02-17 00:35:58 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the mother function
+ *
+ * Return: return to initial value
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
